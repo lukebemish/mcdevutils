2e83c06305357b849f666cdba3f168a32b706081
Luke Bemish <lukebemish@gmail.com>
2022-06-29 19:21:15 -0500

    Fix issues with version

--- src/main/groovy/io/github/lukebemish/mcdevutils/impl/gradle/Constants.groovy
+++ src/main/groovy/io/github/lukebemish/mcdevutils/impl/gradle/Constants.groovy
@@ -9,5 +9,5 @@ class Constants {
 
 
     public static final String CACHE_FOLDER = "mcdevutil"
-    public static final String VERSION = "0.1.0"
+    public static final String VERSION = Constants.class.package.implementationVersion
 }
